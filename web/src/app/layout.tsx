c7c4640e5582293f04ff11c0880d113f3bc50032
Foxie Flakey <foxieflakey@gmail.com>
2026-04-24 17:34:08 +0700

    Add basic UI shape

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,4 +1,7 @@
+import Header from "@/components/header";
 import "./globals.css";
+
+import styles from "./page.module.css"
 
 export default function RootLayout({
   children,
@@ -7,7 +10,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        <div className={ styles.content }>{children}</div>
+        <div className={ styles.filler } />
+      </body>
     </html>
   );
 }
