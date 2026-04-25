ae5f5678a3047bec74ab4b1b65251e93e469f564
Foxie Flakey <foxieflakey@gmail.com>
2026-04-25 09:09:03 +0700

    Update root page to use UserContext for retrieving user info instead of doing on its own

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,49 +1,25 @@
 'use client';
 
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { API_URL, WEB_URL } from "./config";
-import * as Backend from "@/lib/backend/me";
-import { getSessionToken } from "./session";
-import { ErrorCode } from "@/lib/backend/error";
 import ProfileCard from "@/components/profile_card";
-import { UserInfo } from "@/lib/backend/lib";
 import { UserContext } from "@/components/user_provider";
 
 export default function Home() {
-  const [ loading, setLoading ] = useState(true);
-  const [ error, setError ] = useState<string | null>(null);
-  const [ info, setInfo ] = useState<UserInfo | null>(null);
-  const { token } = useContext(UserContext)
+  const { token, userInfo } = useContext(UserContext)
   
-  useEffect(() => {
-    (async () => {
-      if (token == null) {
-        setError("You're not logged in, please login")
-        setLoading(false)
-        return
-      }
-      
-      const me = await Backend.me(token)
-      if (!me.ok) {
-        if (me.value.code == ErrorCode.InvalidSessionToken) {
-          setError("You're not logged in, please login")
-        } else {
-          setError("Cannot retrieve your user info: " + me.value.message)
-        }
-        setLoading(false)
-        return
-      }
-      
-      setInfo(me.value)
-      setLoading(false)
-    })()
-  }, [token]);
-  
-  var card = <ProfileCard userInfo={ info } isLoading={ loading } />
-  if (!loading && error != null) {
+  const info = userInfo?.ok ? userInfo?.value : null
+  var card = <ProfileCard userInfo={ info } isLoading={ userInfo == null } />
+  if (userInfo != null && !userInfo.ok) {
     // Error occured
     card = <p>
-      Sorry, there was an error while loading user profile: { error }
+      Sorry, there was an error while loading user profile: { userInfo.value.message }
+    </p>
+  }
+  
+  if (token == null) {
+    card = <p>
+      You're not logged in please login
     </p>
   }
   
