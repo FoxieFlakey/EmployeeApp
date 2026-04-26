25fe4b49b9ac06f59ba687dedf47c233fc6f60d5
Foxie Flakey <foxieflakey@gmail.com>
2026-04-26 23:03:40 +0700

    Prefix the endpoints for API

--- web/src/app/config.tsx
+++ web/src/app/config.tsx
@@ -1,4 +1,4 @@
 export const WEB_URL = `http://${ process.env.NEXT_PUBLIC_WEB_HOSTNAME }:${ process.env.NEXT_PUBLIC_WEB_PORT }`;
-export const API_URL = `http://${ process.env.NEXT_PUBLIC_API_HOSTNAME }:${ process.env.NEXT_PUBLIC_API_PORT }`;
+export const API_URL = `http://${ process.env.NEXT_PUBLIC_API_HOSTNAME }:${ process.env.NEXT_PUBLIC_API_PORT }/v1`;
 
 
